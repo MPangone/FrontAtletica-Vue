1b7cb614457547b111cb6a6bdb3d49ba4a673d46
João Pinhel <57845354+joaopinhel@users.noreply.github.com>
2023-11-21 00:20:47 -0300

    login e cadastro

---
+++ src/componentes/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div style="display: flex; justify-content: center; align-items: center; height: 100vh; background-color: #e0e0e0;">
+    <div
+      style="max-width: 400px; margin: 0 auto; padding: 20px; background-color: #ffffff; border: 1px solid #b0b0b0; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+      <h2 style="text-align: center; margin-bottom: 20px; color: #007bff;">Login</h2>
+      <form @submit.prevent="login">
+        <div style="margin-bottom: 20px;">
+          <label for="usuario"
+            style="display: block; margin-bottom: 5px; font-weight: bold; color: #333;">Usuário:</label>
+          <input type="text" v-model="usuario" required
+            style="width: 100%; padding: 8px; box-sizing: border-box; border: 1px solid #b0b0b0; border-radius: 4px;">
+        </div>
+
+        <div style="margin-bottom: 20px;">
+          <label for="senha" style="display: block; margin-bottom: 5px; font-weight: bold; color: #333;">Senha:</label>
+          <input type="password" v-model="senha" required
+            style="width: 100%; padding: 8px; box-sizing: border-box; border: 1px solid #b0b0b0; border-radius: 4px;">
+        </div>
+
+        <button type="submit"
+          style="background-color: #007bff; color: #fff; padding: 10px; border: none; border-radius: 4px; cursor: pointer; font-size: 16px;">Login</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      usuario: '',
+      senha: '',
+    };
+  },
+
+  methods: {
+    login() {
+      // Lógica de autenticação aqui
+      const storedUserInformation = localStorage.getItem('userInformation');
+
+      if (storedUserInformation) {
+        const userInformation = JSON.parse(storedUserInformation);
+
+        if (this.usuario === userInformation.usuario && this.senha === userInformation.senha) {
+          // Defina isAuthenticated como true após o login bem-sucedido
+          this.$emit('authenticated', true);
+
+          // Redirecione para a página "esporte" em vez de "bem-vindo"
+          this.$router.push({ name: 'esporte' });
+        } else {
+          alert('Credenciais inválidas. Tente novamente.');
+        }
+      } else {
+        alert('Nenhuma informação de cadastro encontrada. Faça o cadastro primeiro.');
+      }
+    },
+  },
+};
+</script>
